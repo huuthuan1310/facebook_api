8690b8a0441b8b60133377ca83941a0adb4c4282
Nguyen Huu Thuan <leonguyen236@gmail.com>
2017-10-09 13:25:44 +0700

    updated animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,25 @@ import { Http, Response } from '@angular/http';
 import linkifyStr from 'linkifyjs/string';
 import { MatDialog } from '@angular/material';
 import { DialogComponent } from './dialog/dialog.component';
+import { trigger, style, animate, transition } from '@angular/animations';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({ opacity: 0 }),
+          animate('500ms', style({ opacity: 1 }))
+        ]),
+        transition(':leave', [
+          style({ opacity: 1 }),
+          animate('500ms', style({ opacity: 0 }))
+        ])
+      ]
+    )
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -15,21 +31,54 @@ export class AppComponent implements OnInit {
   paging: any;
   token = 'EAAAAUaZA8jlABANGd6IpXk0e6g9IzoGRX6vxlZATbnMOq6NmZBKr70etmzPolCRZBwXBjjtYrvXQ0noH1sWKeFgQRiRjOPBEJZBOQZBV732u6L24LsZCffk0CnAPkz8dF9fLlk0YwohMrK4nXXEbo4CORIIQloW5OF5shcCpePKg5rTNmbmkU8B';
   fields = 'full_picture,from,caption,description,message,updated_time,likes,type,source,link,created_time,shares,status_type,with_tags,message_tags,comments,permalink_url';
-  limit = 10;
+  limit = 12;
   loading = true;
+  facebookId = 'leothuan';
+  loadMore = false;
   constructor(
     public http: Http,
     private cd: ChangeDetectorRef,
     public dialog: MatDialog) { }
   ngOnInit() {
-    this.firstLoad();
+    this.firstLoad(this.facebookId);
   };
 
   onScroll() {
-    let page = this.paging.next.replace('limit=100', 'limit=30');
-    this.http.get(page).subscribe(data => {
+    if (this.paging) {
+      this.loadMore = true;
+      let page = this.paging.next.replace('limit=100', 'limit=30');
+      this.http.get(page).subscribe(data => {
+        // Read the result field from the JSON response.
+        let obj = this.list;
+        var options = {/* … */ };
+        this.paging = data.json().paging;
+        for (let i = 0; i < data.json().data.length; i++) {
+          let item = data.json().data[i];
+          item.message ? item.message = linkifyStr(item.message, options) : '';
+          obj.push(item);
+        }
+        this.list = obj;
+        this.cd.markForCheck();
+        this.loadMore = false;
+        console.log(this.list);
+      });
+    } else {
+      console.log('hết!');
+    }
+  }
+
+  onScrollUp() {
+    if (this.list.length > this.limit) {
+      // this.loading = true;
+      this.firstLoad(this.facebookId);
+    }
+  }
+
+  firstLoad(facebookId, scrollTop?) {
+    // Make the HTTP request:
+    this.http.get('https://graph.facebook.com/v2.10/' + facebookId + '/feed?fields=' + this.fields + '&limit=' + this.limit + '&access_token=' + this.token).subscribe(data => {
       // Read the result field from the JSON response.
-      let obj = this.list;
+      let obj = [];
       var options = {/* … */ };
       this.paging = data.json().paging;
       for (let i = 0; i < data.json().data.length; i++) {
@@ -38,32 +87,8 @@ export class AppComponent implements OnInit {
         obj.push(item);
       }
       this.list = obj;
-      this.cd.markForCheck();
-      console.log(this.list);
-    });
-  }
-
-  onScrollUp() {
-    if (this.list.length > this.limit) {
-      this.loading = true;
-      this.firstLoad();
-    }
-  }
-
-  firstLoad() {
-    // Make the HTTP request:
-    this.http.get('https://graph.facebook.com/v2.10/1173636692750000/feed?fields=' + this.fields + '&limit=' + this.limit + '&access_token=' + this.token).subscribe(data => {
-      // Read the result field from the JSON response.
-      let obj = [];
-      var options = {/* … */ };
-      this.paging = data.json().paging;
-      for (let i = 0; i < data.json().data.length; i++) {
-        let item = data.json().data[i];
-        item.message = linkifyStr(item.message, options);
-        obj.push(item);
-      }
-      this.list = obj;
       this.loading = false;
+      scrollTop ? this.scrollToTop() : '';
       console.log(data.json());
     });
   }
@@ -77,5 +102,18 @@ export class AppComponent implements OnInit {
       console.log('The dialog was closed');
       // this.animal = result;
     });
+  }
+
+  submit(facebookId) {
+    if (facebookId) {
+      this.facebookId = facebookId;
+      this.firstLoad(this.facebookId, true);
+
+    }
+  }
+
+  scrollToTop() {
+    $("#content").animate({ scrollTop: 0 }, 500, 'swing', function () {
+    });;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { InfiniteScrollModule } from 'angular2-infinite-scroll';
@@ -8,7 +9,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 // import { AppProvider } from './app.provider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatInputModule, MatMenuModule, MatIconModule } from '@angular/material';
 // Import your library
 import { Ng2EmojiModule } from 'ng2-emoji';
 import { PerfectScrollbarModule, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
@@ -24,10 +25,14 @@ const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule,
     DialogModule,
     HttpClientModule,
     HttpModule,
